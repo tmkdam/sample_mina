804ae0cc2414a1d7f841e0dcf195118ceb92a8d7
Taha <tmukaddam@sonichealthcareusa.com>
2016-08-25 09:38:31 -0500

    added rvm support

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+  invoke :'rvm:use[ruby-2.2.2@default]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
