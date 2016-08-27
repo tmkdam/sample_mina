75ac29158883b63a1b3d7aca2bfe5215c45e6d8d
Taha <tmukaddam@sonichealthcareusa.com>
2016-08-27 10:12:53 -0500

    udpated puma for dev vs prodution env

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,5 @@
 # SHUSA /config/puma.rb
+# Tmkdam
 
 # Change to match your CPU core count
 # Specifies the number of `workers` to boot in clustered mode.
@@ -17,44 +18,47 @@ min_threads_count = Integer(ENV['RAILS_MIN_THREADS'] || 1)
 max_threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads min_threads_count, max_threads_count
 
-# Run in background
-daemonize true
+# Use following in production
+if Rails.env.production?
+	# Run in background
+	daemonize true
 
-app_name = "sample_mina"
-app_dir = "/var/www/#{app_name}"
-shared_dir = "#{app_dir}/shared"
+	app_name = "sample_mina"
+	app_dir = "/var/www/#{app_name}"
+	shared_dir = "#{app_dir}/shared"
 
-# Specifies the `environment` that Puma will run in.
-# Default to production
-#
-rails_env = ENV['RAILS_ENV'] || "production"
-environment rails_env
+	# Specifies the `environment` that Puma will run in.
+	# Default to production
+	#
+	rails_env = ENV['RAILS_ENV'] || "production"
+	environment rails_env
 
-# Test for enviorment before enabling
-preload_app!
+	# Test for enviorment before enabling
+	preload_app!
 
-# Set up socket location
-bind "unix:///var/www/#{app_name}/shared/tmp/sockets/puma.sock"
+	# Set up socket location
+	bind "unix:///var/www/#{app_name}/shared/tmp/sockets/puma.sock"
 
-# Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+	# Logging
+	stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
-# Set master PID and state locations
-pidfile "/var/www/#{app_name}/shared/tmp/pids/puma.pid"
-stdout_redirect "/var/www/#{app_name}/shared/tmp/log/stdout", "/var/www/#{app_name}/shared/tmp/log/stderr"
+	# Set master PID and state locations
+	pidfile "/var/www/#{app_name}/shared/tmp/pids/puma.pid"
+	stdout_redirect "/var/www/#{app_name}/shared/tmp/log/stdout", "/var/www/#{app_name}/shared/tmp/log/stderr"
 
-state_path "#{shared_dir}/pids/puma.state"
-activate_control_app
+	state_path "#{shared_dir}/pids/puma.state"
+	activate_control_app
 
-# The code in the `on_worker_boot` will be called if you are using
-# clustered mode by specifying a number of `workers`. After each worker
-# process is booted this block will be run, if you are using `preload_app!`
-# option you will want to use this block to reconnect to any threads
-# or connections that may have been created at application boot, Ruby
-# cannot share connections between processes.
-# 
-on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{shared_dir}/config/database.yml")[rails_env])
+	# The code in the `on_worker_boot` will be called if you are using
+	# clustered mode by specifying a number of `workers`. After each worker
+	# process is booted this block will be run, if you are using `preload_app!`
+	# option you will want to use this block to reconnect to any threads
+	# or connections that may have been created at application boot, Ruby
+	# cannot share connections between processes.
+	# 
+	on_worker_boot do
+	  require "active_record"
+	  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+	  ActiveRecord::Base.establish_connection(YAML.load_file("#{shared_dir}/config/database.yml")[rails_env])
+	end
 end
