f986a611ab3585a9402e72a3c40f07cf53bc76bc
Taha <tmukaddam@sonichealthcareusa.com>
2016-08-26 22:40:58 -0500

    added patches to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,6 @@ require "mina/hooks"
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
-after_mina :"puma:phased_restart"
 
 set :user, 'deployer'
 set :domain, 'localhost'
@@ -95,6 +94,8 @@ task :deploy => :environment do
   end
 end
 
+after_mina :"puma:phased_restart"
+puts after_mina_tasks.inspect
 # namespace :puma do
 #   desc "Start the application"
 #   task :start do
